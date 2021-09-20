2f9dad83c12ec0f2b285fd82bcbbb41af0fface2
DOUGBOWMER <dougbowmer3@gmail.com>
2021-09-20 01:09:11 -0400

    git commit

---
+++ Basic_HTML_and Javascipt_1/Basic_HTML_and Javascipt_1/Basic_HTML_and_JavaScript_1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<script>
+alert("This is my new box!");
+</script>
+	
+	
+    
+     </body>
+</html>
---
+++ Basic_HTML_and Javascipt_1/Basic_HTML_and Javascipt_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+
+
+function My_First_Function() {
+		var string = "Kiss me, I'm Irsh";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML=
+		result;
+		}
+	
