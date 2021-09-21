825d3dcc9c4ec07e533b2ab7a101d815960ff97b
DOUGBOWMER <dougbowmer3@gmail.com>
2021-09-21 00:49:47 -0400

    git commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HTML and CSS Projects tech academy
+ this is where i keep all my HTML and CSS files pertaing to techacademy
---
+++ VS CODE Assignment/Hello_World.js
@@ -0,0 +1,8 @@
+var  greeting = 'hello world!';
+console.log('hello world');
+
+var greeting2 = 'welcome to Node.js';
+console.log(greeting2);
+
+var sentence1 = "learning how to debug code with the debugger";
+console.log(sentence1);
